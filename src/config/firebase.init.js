4a2b6031af09c4b1aafef80f649c00f788648a68
Bilkish-akther <bilkishakther921@gmail.com>
2025-03-12 21:16:49 +0600

    Initial commit

---
+++ src/config/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARMneeGPF8ccwDqQqWuh8asrobf9bf_ys",
+  authDomain: "sound-safari-f0811.firebaseapp.com",
+  projectId: "sound-safari-f0811",
+  storageBucket: "sound-safari-f0811.appspot.com",
+  messagingSenderId: "380111442365",
+  appId: "1:380111442365:web:f9595d469eccdcb6e242d4"
+};
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
